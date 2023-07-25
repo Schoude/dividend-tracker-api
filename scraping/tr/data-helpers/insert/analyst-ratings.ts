c38358a279bb4ed8397c63ce0dc794d46fd59578
Marc <marcbaque1311@gmail.com>
2023-07-25 22:06:34 +0200

    [ ADD ] analytics rating insertion

---
+++ scraping/tr/data-helpers/insert/analyst-ratings.ts
@@ -0,0 +1,30 @@
+import { supabase } from '../../../../src/supabase/client.ts';
+import { stockDetails } from '../../output/stock-details.ts';
+
+const analystRatings = stockDetails.map((stock) => {
+  const isin = stock.isin;
+
+  return {
+    isin,
+    recommendations_buy: stock.analystRating.recommendations.buy,
+    recommendations_hold: stock.analystRating.recommendations.hold,
+    recommendations_outperform: stock.analystRating.recommendations.outperform,
+    recommendations_underperform:
+      stock.analystRating.recommendations.underperform,
+    target_price_average: Number(
+      (stock.analystRating.targetPrice.average).toFixed(2),
+    ),
+    target_price_high: Number(
+      (stock.analystRating.targetPrice.high).toFixed(2),
+    ),
+    target_price_low: Number((stock.analystRating.targetPrice.low).toFixed(2)),
+  };
+});
+
+const { data, error } = await supabase.from('analyst_ratings').insert(
+  analystRatings,
+)
+  .select('isin');
+
+console.log({ data });
+console.log({ error });
--- src/supabase/types.ts
+++ src/supabase/types.ts
@@ -6,9 +6,52 @@ export type Json =
   | { [key: string]: Json | undefined }
   | Json[];
 
+
 export interface Database {
   public: {
     Tables: {
+      analyst_ratings: {
+        Row: {
+          created_at: string;
+          id: number;
+          isin: string | null;
+          recommendations_buy: number | null;
+          recommendations_hold: number | null;
+          recommendations_outperform: number | null;
+          recommendations_underperform: number | null;
+          target_price_average: number | null;
+          target_price_high: number | null;
+          target_price_low: number | null;
+          updated_at: string;
+        };
+        Insert: {
+          created_at?: string;
+          id?: number;
+          isin?: string | null;
+          recommendations_buy?: number | null;
+          recommendations_hold?: number | null;
+          recommendations_outperform?: number | null;
+          recommendations_underperform?: number | null;
+          target_price_average?: number | null;
+          target_price_high?: number | null;
+          target_price_low?: number | null;
+          updated_at?: string;
+        };
+        Update: {
+          created_at?: string;
+          id?: number;
+          isin?: string | null;
+          recommendations_buy?: number | null;
+          recommendations_hold?: number | null;
+          recommendations_outperform?: number | null;
+          recommendations_underperform?: number | null;
+          target_price_average?: number | null;
+          target_price_high?: number | null;
+          target_price_low?: number | null;
+          updated_at?: string;
+        };
+        Relationships: [];
+      };
       portfolios: {
         Row: {
           created_at: string;
