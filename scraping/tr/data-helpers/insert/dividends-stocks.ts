c590c60cb1e6d7126255d579657ba1644e838268
Marc <marcbaque1311@gmail.com>
2023-07-31 21:44:35 +0200

    [ UPDATE ] only insert unknown dividends

--- scraping/tr/data-helpers/insert/dividends-stocks.ts
+++ scraping/tr/data-helpers/insert/dividends-stocks.ts
@@ -6,42 +6,53 @@ import stocks from '../../../../stocks.json' assert { type: 'json' };
 type RowInsertDividendsStock =
   Database['public']['Tables']['dividends_stock']['Insert'];
 
-const inserts: RowInsertDividendsStock[][] = await Promise.all(
-  stocks.map(async (stock) => {
-    const dbStock = await supabase
-      .from('stocks')
-      .select('id, isin')
-      .eq('intl_symbol', stock.symbol)
-      .single();
+const inserts: RowInsertDividendsStock[] = [];
 
-    if (dbStock.status > 200) {
-      console.log(
-        bgRed(bold('Error finding the stock')),
-        stock.stock_full_name,
-        bold(stock.symbol),
-      );
+for (const stock of stocks) {
+  const dbStock = await supabase
+    .from('stocks')
+    .select('id, isin')
+    .eq('intl_symbol', stock.symbol)
+    .single();
 
-      return;
+  if (dbStock.status > 200) {
+    console.log(
+      bgRed(bold('Error finding the stock')),
+      stock.stock_full_name,
+      bold(stock.symbol),
+    );
+
+    continue;
+  }
+
+  for (const dividend of stock.dividends) {
+    const knownDividend = await supabase
+      .from('dividends_stock')
+      .select('id')
+      .eq('isin', dbStock.data?.isin!)
+      .eq('payment_date_iso', dividend.payout_date)
+      .eq('ex_date_iso', dividend.ex_dividend_date);
+
+    if (knownDividend.error || knownDividend.data.length > 0) {
+      continue;
     }
 
-    return stock.dividends.map((dividend) => {
-      return {
-        isin: dbStock.data?.isin!,
-        info: dividend.info === '' ? null : dividend.info,
-        amount: Number(dividend.cash_amount.toFixed(2)),
-        ex_date_iso: dividend.ex_dividend_date,
-        ex_date_unix: new Date(dividend.ex_dividend_date).getTime(),
-        payment_date_iso: dividend.payout_date,
-        payment_date_unix: new Date(dividend.payout_date).getTime(),
-        stock_id: dbStock.data?.id!,
-      } as RowInsertDividendsStock;
+    inserts.push({
+      isin: dbStock.data?.isin!,
+      info: dividend.info === '' ? null : dividend.info,
+      amount: Number(dividend.cash_amount.toFixed(2)),
+      ex_date_iso: dividend.ex_dividend_date,
+      ex_date_unix: new Date(dividend.ex_dividend_date).getTime(),
+      payment_date_iso: dividend.payout_date,
+      payment_date_unix: new Date(dividend.payout_date).getTime(),
+      stock_id: dbStock.data?.id!,
     });
-  }).filter((value) => value != null) as RowInsertDividendsStock[],
-) as RowInsertDividendsStock[][];
+  }
+}
 
 const dividendsStockResult = await supabase
   .from('dividends_stock')
-  .insert(inserts.flat())
+  .insert(inserts)
   .select('isin, payment_date_iso');
 
 console.log(dividendsStockResult.data);
